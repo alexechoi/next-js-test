cb14b2daec4b055a2a65f2d42f457d62dd1150bd
alexchvod <uceiac8@ucl.ac.uk>
2024-10-30 19:58:24 +0000

    Initial setup with Next.js, TypeScript, ESLint, and Tailwind CSS

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl">Alex</h1>
+    </main>
+  );
+}
